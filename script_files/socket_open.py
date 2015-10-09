9026b819acd2ec8cc5360313380cfc8b52bbf723
Greg <greg.distefano+github@gmail.com>
2015-10-10 00:39:44 +0200

    Added socket open test

---
+++ script_files/socket_open.py
@@ -0,0 +1,26 @@
+from scripts import Scripts
+from socket import socket
+
+class OpenSocket(Scripts):
+    host = ""
+    ports = []
+
+    def __init__(self, host, ports, **kwargs):
+        self.description = "Check if ports are open"
+        self.host = host
+        self.ports = ports
+
+        #pass remaining arguments to the parent class
+        super(OpenSocket, self).__init__(**kwargs)
+
+    def do_test(self):
+        for port in self.ports:
+            try:
+                s = socket()
+                s.settimeout(1)
+                s.connect( (self.host, int(port)) )
+            except Exception, e:
+                fail_str = "Port: %d is closed" % (port)
+                self.failed(fail_str)
+            else:
+                self.passed()
